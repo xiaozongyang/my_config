926e39ed129fb02d2f433d8a4336bcffb2596e08
Xiao Zongyang <xiaozongyang_bupt@163.com>
2017-05-07 23:33:16 +0800

    add script to replace original zshrc with mine

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 # clone oh-my-zsh and change shell to zsh
 cd ~ && sh -c "$(curl -fsSL \
     https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /usr/bin/zsh
+# replace original .zshrc with mine and reload it
+mv ~/.zshrc ~/.zshrc.bak
+ln -s .zshrc ~/.zshrc
+source ~/.zshrc
+
 # install vim-plug plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
